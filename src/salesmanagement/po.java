febb4f6f8b692ad11fd4c25320061f0ab104b157
char-kuey-teow <charlottechenzishan@gmail.com>
2025-04-14 15:54:27 +0800

    add pr and po

---
+++ src/salesmanagement/po.java
@@ -0,0 +1,25 @@
+package salesmanagement;
+
+/**
+ *
+ * @author charlotte
+ */
+import java.util.Date;
+
+class hehe {
+    private String po_id;
+    private String item_id;
+    private Date order_date;
+    private Date delivery_date;
+    private String supplier_id;
+    private int sm_id;
+    
+}
+public class po {
+    public static void main(String[] args) {
+        poFrame p = new poFrame();
+        p.setLocationRelativeTo(null); //set frame in center
+        p.setVisible(true);
+    }
+    
+}
---
+++ src/salesmanagement/pr.java
@@ -0,0 +1,18 @@
+package salesmanagement;
+
+import java.util.Date;
+
+/**
+ *
+ * @author charlotte
+ */
+public class pr {
+    private String pr_id;
+    private String item_id;
+    private int total;
+    private Date required_date;
+    private String pr_quantity;
+    private String supplier_id;
+    private int sm_id;
+    private Date current_date;
+}
